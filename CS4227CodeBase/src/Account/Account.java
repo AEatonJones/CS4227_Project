5c0be0df64f1fc39ecaa961c7108ac5a3e02620c
HenryMaugh <15141608@studentmail.ul.ie>
2018-10-23 12:09:02 +0100

    Updated Account

--- CS4227CodeBase/src/Account/Account.java
+++ CS4227CodeBase/src/Account/Account.java
@@ -1,18 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Account;
 
-/**
- *
- * @author aaron
- */
 public class Account {
 
-    public Account() {
-
+    private String firstname, surname, email, password, number;
+    
+    /**
+     * Constructor for Account Object
+     * @param firstname
+     * @param surname
+     * @param email
+     * @param password
+     * @param number
+     */
+    public Account(String firstname, String surname, String email, String password, String number) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.number = number;
     }
     
+        /**
+        * Getter which gets the customers first name.
+        * @return First name of customer.
+        */
+        public String getFirstName() {
+        return this.firstname;
+        }
+        
+        /**
+        * Getter which gets the customers first name.
+        * @return  Surname of customer.
+        */
+        public String getSurname() {
+            return this.surname;
+        }
+    
+        /**
+        * Getter which gets the customers first name.
+        * @return Email of customer.
+        */   
+        public String getEmail() {
+        return this.email;
+        }
+        
+        /**
+        * Getter which gets the customers first name.
+        * @return Password of customer.
+        */
+        public String getPassword() {
+        return this.password;
+        }
+        
+        /**
+        * Getter which gets the customers first name.
+        * @return Phone number of customer.
+        */
+        public String getNumber() {
+        return this.number;
+        }
 }
