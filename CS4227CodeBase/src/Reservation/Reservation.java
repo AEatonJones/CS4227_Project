8cd50e99b53ed4320e2cd19f3a977c9b06b776b4
Jack Coffey <15147827@studentmail.ul.ie>
2018-10-24 22:51:24 +0100

    Reservations
    Reservation package, class and tests implemented

---
+++ CS4227CodeBase/src/Reservation/Reservation.java
@@ -0,0 +1,36 @@
+package Reservation;
+
+import java.io.FileWriter;
+
+public class Reservation 
+{
+    private static final String DBNAME = "src/resources/reservations.txt"; 
+    
+    private String accountID;
+    private String startDate;
+    private int noNights;
+    private float cost;
+    
+    private Reservation(String accountID, String startDate, int noNights, float cost) {
+        this.accountID = accountID;
+        this.startDate = startDate;
+        this.noNights = noNights;
+        this.cost = cost;
+    }
+    
+    public static Reservation makeReservation(String accountID, String startDate, int noNights, float cost) throws Exception {
+        Reservation reservation = new Reservation(accountID, startDate, noNights, cost);
+        
+        FileWriter writer = new FileWriter(DBNAME, true);
+        writer.append(reservation.toString());
+        writer.close();
+        
+        return reservation;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return (accountID + "," + startDate + "," + noNights + "," + String.format("%.2f", cost) + "\n");
+    }
+}
---
+++ CS4227CodeBase/test/Reservation/ReservationTest.java
@@ -0,0 +1,31 @@
+package Reservation;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.fail;
+
+public class ReservationTest 
+{
+    private Reservation reservation = null;
+    
+    @Before
+    @Test
+    public void makeValidReservationTest(){
+        
+        try {
+            reservation = Reservation.makeReservation("101", "10/11/12", 10, 60.0f);
+            if(reservation == null)
+                fail("Reservation should not be null.");
+        } catch(Exception e) {
+            e.printStackTrace();
+            fail("Should not throw exception.");
+        }
+    }
+    
+    @Test
+    public void reservationToStringTest()
+    {
+        assertEquals("101,10/11/12,10,60.00\n", reservation.toString());
+    }
+}
