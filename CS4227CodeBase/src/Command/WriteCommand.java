f1e721db0d4518741df2230b5a1d02d77d0695b2
HenryMaugh <15141608@studentmail.ul.ie>
2018-10-31 14:48:51 +0000

    Further Command progress

--- CS4227CodeBase/src/Command/WriteCommand.java
+++ CS4227CodeBase/src/Command/WriteCommand.java
@@ -5,6 +5,16 @@
  */
 package Command;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author hmaug
@@ -19,12 +29,34 @@ public class WriteCommand implements Command {
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try(FileWriter filewriter = new FileWriter("src/resources/WriteCommandTest.txt",true)){
+            filewriter.write("\nwriting");
+            filewriter.close();
+        
+    }   catch (IOException ex) {
+            Logger.getLogger(WriteCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
     @Override
     public void undo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/resources/WriteCommandTest.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/resources/WriteCommandTest.txt"));
+            String undo = "remove";
+            String currentLine;
+            while((currentLine = reader.readLine()) != null){
+                String trimmedLine = currentLine.trim();
+                if(trimmedLine.equals(undo)) continue;
+                    writer.write(currentLine + System.getProperty("line.separator"));
+                }
+            writer.close();
+            reader.close();
+            } catch (IOException ex) {
+            Logger.getLogger(WriteCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+        
+        
     }
     
 }
--- CS4227CodeBase/test/UseCases/UI/CommandTest.java
+++ CS4227CodeBase/test/UseCases/UI/CommandTest.java
@@ -32,14 +32,21 @@ public class CommandTest {
             cmd.execute();
             BufferedReader reader = new BufferedReader(new FileReader("src/resources/WriteCommandTest.txt"));
             assertEquals("Test1", reader.readLine());
-            assertEquals("WriteCommand writing", reader.readLine()); //In writeCommand, have it write to file the string on the line
+            assertEquals("writing", reader.readLine()); //In writeCommand, have it write to file the string on the line
             reader.close();
             
             cmd.undo();
+            
+            while((reader.readLine()) != null){
+                assertEquals(false,reader.readLine().equals("writing"));
+            }
+            
+            reader.close();
             //New reader, loop until end of file, assertEquals(false, reader.readLine().equals("WriteCommand writing"));, 
             //reader.close()
         }
         catch(Exception e){
+            e.printStackTrace();
             fail();
         }
     }
